1cc301d4d0dd4386f12d54cd215cc233c10ebc17
ScrollFusionAdmin <scrollfusion@gmail.com>
2017-02-25 14:28:58 +0000

    Undo temporary changes from last commit

--- CompiledLang/dependencyresolver/dependencyresolver.cpp
+++ CompiledLang/dependencyresolver/dependencyresolver.cpp
@@ -7,7 +7,6 @@ std::unordered_multimap<std::string, FunctionPointer> resolveDependencies() {
 	dependencies.emplace("println", FunctionPointer("dprintln", DataType::VOID, { DataType::DOUBLE }, new NodeNative("dprintln", FilePos(0U, 0U, ""))));
 	dependencies.emplace("println", FunctionPointer("fprintln", DataType::VOID, { DataType::FLOAT }, new NodeNative("fprintln", FilePos(0U, 0U, ""))));
 	dependencies.emplace("println", FunctionPointer("bprintln", DataType::VOID, { DataType::BOOL }, new NodeNative("bprintln", FilePos(0U, 0U, ""))));
-	dependencies.emplace("gendouble", FunctionPointer("gendouble", DataType::DOUBLE, { }, new NodeNative("dldc 3.14159265", FilePos(0U, 0U, ""))));
 	
 	return dependencies;
 }
